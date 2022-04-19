413d081d10759267a4d17bc341159add67ae4873
J.M Winbush <102334772+jwinbush@users.noreply.github.com>
2022-04-19 13:16:15 -0400

    Add files via upload

--- ProjectFinal2/includes/banner.php
+++ ProjectFinal2/includes/banner.php
@@ -1,77 +1,75 @@
   <!-- Each image is 350px by 233px -->
 
 
-  <div class="photobanner" >
+  <div class="photobanner">
 
-    <a href="deals.php"><img class="first" height="180" src="https://www.pngitem.com/pimgs/m/307-3078022_sega-game-gear-wb-hd-png-download.png"> </a>
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/en/c/cd/Marioparty1.jpg"></a>
+    <a href="productdetails.php?id=23"><img class="first" height="190" src="https://www.pngitem.com/pimgs/m/307-3078022_sega-game-gear-wb-hd-png-download.png"></a>
+    <a href="productdetails.php?id=25"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/c/cd/Marioparty1.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://segaretro.org/images/2/2f/MS.Pacman_title.png"></a>
+    <a href="productdetails.php?id=19"><img height="190" src="https://segaretro.org/images/2/2f/MS.Pacman_title.png"></a>
+    <a href="productdetails.php?id=31"><img height="190" src="https://m.media-amazon.com/images/I/51JV6YK81FL.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/commons/c/c6/Nintendo_Gameboy.jpg"></a>
+    <a href="productdetails.php?id=29"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/e/e6/Crash_Bandicoot_2_Cortex_Strikes_Back_Game_Cover.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://www.pngkey.com/png/full/317-3172188_n64-nintendo-64.png"></a>
+    <a href="productdetails.php?id=2"><img height="190" src="https://www.pngkey.com/png/full/317-3172188_n64-nintendo-64.png"></a>
 
-    <a href="deals.php"><img height="180" src="https://icon-library.com/images/playstation-1-icon/playstation-1-icon-13.jpg"></a>
+    <a href="productdetails.php?id=20"><img height="190" src="https://icon-library.com/images/playstation-1-icon/playstation-1-icon-13.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/en/4/4a/Pokemon_Yellow.png"></a>
-    <a href="deals.php"><img height="180" src="https://www.pngitem.com/pimgs/m/307-3078022_sega-game-gear-wb-hd-png-download.png"> </a>
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/en/c/cd/Marioparty1.jpg"></a>
+    <a href="productdetails.php?id=5"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/4/4a/Pokemon_Yellow.png"></a>
+    <a href="productdetails.php?id=24"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/a/ae/DonkeyKong64CoverArt.jpg"></a>
+    <a href="productdetails.php?id=12"><img height="190" src="https://halo.wiki.gallery/images/6/6a/Halo_Combat_Evolved_cover.png"></a>
 
-    <a href="deals.php"><img height="180" src="https://segaretro.org/images/2/2f/MS.Pacman_title.png"></a>
+    <a href="productdetails.php?id=8"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/a/a1/Mario_Kart_64.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/commons/c/c6/Nintendo_Gameboy.jpg"></a>
+    <a href="productdetails.php?id=22"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/4/4c/Pok%C3%A9mon_box_art_-_Gold_Version.png"></a>
 
-    <a href="deals.php"><img height="180" src="https://www.pngkey.com/png/full/317-3172188_n64-nintendo-64.png"></a>
+    <a href="productdetails.php?id=9"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/3/36/GoldenEye007box.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://icon-library.com/images/playstation-1-icon/playstation-1-icon-13.jpg"></a>
+    <a href="productdetails.php?id=20"><img height="190" src="https://icon-library.com/images/playstation-1-icon/playstation-1-icon-13.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/en/4/4a/Pokemon_Yellow.png"></a>
-    <a href="deals.php"><img height="180" src="https://www.pngitem.com/pimgs/m/307-3078022_sega-game-gear-wb-hd-png-download.png"> </a>
+    <a href="productdetails.php?id=21"><img height="190" src="https://www.grtoys.com/components/com_virtuemart/shop_image/product/full/717uhFf53L_AC_SL1500_5ed9383825fb7_r1.jpg"></a>
+    <a href="productdetails.php?id=26"><img height="190" src="http://s3.amazonaws.com/digitaltrends-uploads-prod/2015/04/original-xbox.png"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/en/c/cd/Marioparty1.jpg"></a>
+    <a href="productdetails.php?id=23"><img height="190" src="https://www.pngitem.com/pimgs/m/307-3078022_sega-game-gear-wb-hd-png-download.png"></a>
+    <a href="productdetails.php?id=25"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/c/cd/Marioparty1.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://segaretro.org/images/2/2f/MS.Pacman_title.png"></a>
+    <a href="productdetails.php?id=19"><img height="190" src="https://segaretro.org/images/2/2f/MS.Pacman_title.png"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/commons/c/c6/Nintendo_Gameboy.jpg"></a>
+    <a href="productdetails.php?id=28"><img height="190" src="https://m.media-amazon.com/images/M/MV5BYmJlY2RlYjEtNWU3Yy00YTRlLTllMzUtZmYwY2NkMzgxZmFmXkEyXkFqcGdeQXVyNjM2NTM3ODA@._V1_.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://www.pngkey.com/png/full/317-3172188_n64-nintendo-64.png"></a>
+    <a href="productdetails.php?id=2"><img height="190" src="https://www.pngkey.com/png/full/317-3172188_n64-nintendo-64.png"></a>
 
-    <a href="deals.php"><img height="180" src="https://icon-library.com/images/playstation-1-icon/playstation-1-icon-13.jpg"></a>
+    <a href="productdetails.php?id=20"><img height="190" src="https://icon-library.com/images/playstation-1-icon/playstation-1-icon-13.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/en/4/4a/Pokemon_Yellow.png"></a>
-    <a href="deals.php"><img height="180" src="https://www.pngitem.com/pimgs/m/307-3078022_sega-game-gear-wb-hd-png-download.png"> </a>
+    <a href="productdetails.php?id=5"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/4/4a/Pokemon_Yellow.png"></a>
+    <a href="productdetails.php?id=24"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/a/ae/DonkeyKong64CoverArt.jpg"></a>
+    <a href="productdetails.php?id=12"><img height="190" src="https://halo.wiki.gallery/images/6/6a/Halo_Combat_Evolved_cover.png"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/en/c/cd/Marioparty1.jpg"></a>
+    <a href="productdetails.php?id=8"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/a/a1/Mario_Kart_64.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://segaretro.org/images/2/2f/MS.Pacman_title.png"></a>
+    <a href="productdetails.php?id=22"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/4/4c/Pok%C3%A9mon_box_art_-_Gold_Version.png"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/commons/c/c6/Nintendo_Gameboy.jpg"></a>
+    <a href="productdetails.php?id=9"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/3/36/GoldenEye007box.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://www.pngkey.com/png/full/317-3172188_n64-nintendo-64.png"></a>
+    <a href="productdetails.php?id=20"><img height="190" src="https://icon-library.com/images/playstation-1-icon/playstation-1-icon-13.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://icon-library.com/images/playstation-1-icon/playstation-1-icon-13.jpg"></a>
+    <a href="productdetails.php?id=21"><img height="190" src="https://www.grtoys.com/components/com_virtuemart/shop_image/product/full/717uhFf53L_AC_SL1500_5ed9383825fb7_r1.jpg"></a>
+    <a href="productdetails.php?id=26"><img height="190" src="http://s3.amazonaws.com/digitaltrends-uploads-prod/2015/04/original-xbox.png"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/en/4/4a/Pokemon_Yellow.png"></a>
-    <a href="deals.php"><img height="180" src="https://www.pngitem.com/pimgs/m/307-3078022_sega-game-gear-wb-hd-png-download.png"> </a>
+    <a href="productdetails.php?id=23"><img height="190" src="https://www.pngitem.com/pimgs/m/307-3078022_sega-game-gear-wb-hd-png-download.png"></a>
+    <a href="productdetails.php?id=25"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/c/cd/Marioparty1.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/en/c/cd/Marioparty1.jpg"></a>
+    <a href="productdetails.php?id=19"><img height="190" src="https://segaretro.org/images/2/2f/MS.Pacman_title.png"></a>
 
-    <a href="deals.php"><img height="180" src="https://segaretro.org/images/2/2f/MS.Pacman_title.png"></a>
+    <a href="productdetails.php?id=1"><img height="190" src="https://upload.wikimedia.org/wikipedia/commons/c/c6/Nintendo_Gameboy.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/commons/c/c6/Nintendo_Gameboy.jpg"></a>
+    <a href="productdetails.php?id=2"><img height="190" src="https://www.pngkey.com/png/full/317-3172188_n64-nintendo-64.png"></a>
 
-    <a href="deals.php"><img height="180" src="https://www.pngkey.com/png/full/317-3172188_n64-nintendo-64.png"></a>
+    <a href="productdetails.php?id=20"><img height="190" src="https://icon-library.com/images/playstation-1-icon/playstation-1-icon-13.jpg"></a>
 
-    <a href="deals.php"><img height="180" src="https://icon-library.com/images/playstation-1-icon/playstation-1-icon-13.jpg"></a>
-
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/en/4/4a/Pokemon_Yellow.png"></a>
-    <a href="deals.php"><img height="180" src="https://www.pngitem.com/pimgs/m/307-3078022_sega-game-gear-wb-hd-png-download.png"> </a>
-
-    <a href="deals.php"><img height="180" src="https://upload.wikimedia.org/wikipedia/en/c/cd/Marioparty1.jpg"></a>
-
-    <a href="deals.php"><img height="180" src="https://segaretro.org/images/2/2f/MS.Pacman_title.png"></a>
-
+    <a href="productdetails.php?id=5"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/4/4a/Pokemon_Yellow.png"></a>
+    <a href="productdetails.php?id=26"><img height="190" src="https://upload.wikimedia.org/wikipedia/en/a/ae/DonkeyKong64CoverArt.jpg"></a>
+   
 
 
 
