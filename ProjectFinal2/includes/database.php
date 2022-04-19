fc3346c0917213a508b8398239cb7fa401220e1a
J.M Winbush <102334772+jwinbush@users.noreply.github.com>
2022-04-19 13:20:58 -0400

    Update database.php

--- ProjectFinal2/includes/database.php
+++ ProjectFinal2/includes/database.php
@@ -4,12 +4,6 @@ $host = "localhost:3306";
 $login = "phpuser";
 $password = "phpuser";
 $database = "retrogametwo";
-$category1 = "SELECT * FROM categories WHERE category_id = 1";
-$category2 = "SELECT * FROM categories WHERE category_id = 2";
-$category3 = "SELECT * FROM categories WHERE category_id = 3";
-$category4 = "SELECT * FROM categories WHERE category_id = 4";
-$category5 = "SELECT * FROM categories WHERE category_id = 5";
-
 
 
 $conn = @new mysqli($host, $login, $password, $database);
